3d72672324453489d5625e011de5b85801f1baab
AntoninStudenik <143001456+AntoninStudenik@users.noreply.github.com>
2025-05-13 13:45:07 +0200

    quaries.sql

--- quaries.sql
+++ quaries.sql
@@ -11,12 +11,12 @@ SELECT * FROM zamestnanci WHERE jmeno IN ('Petra', 'Tereza');--vybere vsechny co
 SELECT * FROM zamestnanci WHERE vek = 42;--vybere vsechny komu je 42
 
 -- 14-19: Dotazy s JOIN ON
-SELECT f.jmeno, f.prijmeni, s.nazev FROM fitkari f JOIN skupinovelekce s ON f.skupinovelekce_id = s.id;-- Vybere jméno a příjmení fitkaře a název skupinové lekce, na kterou chodí
-SELECT f.jmeno, f.prijmeni, b.druh FROM fitkari f JOIN bojovesporty b ON f.bojovesporty_id = b.id;-- Vybere jméno a příjmení fitkaře a druh bojového sportu, kterému se věnuje
-SELECT b.druh, z.jmeno, z.prijmeni FROM bojovesporty b JOIN zamestnanci z ON b.trener = CONCAT(z.jmeno, ' ', z.prijmeni);-- Vybere druh bojového sportu a jméno + příjmení trenéra, který ho vede
-SELECT s.nazev, z.jmeno, z.prijmeni FROM skupinovelekce s JOIN zamestnanci z ON s.trener = CONCAT(z.jmeno, ' ', z.prijmeni);-- Vybere název skupinové lekce a jméno + příjmení trenéra, který ji vede
-SELECT f.jmeno AS fitkar, b.trener FROM fitkari f JOIN bojovesporty b ON f.bojovesporty_id = b.id;-- Vybere jméno fitkaře a jméno trenéra bojového sportu, kterému se fitkař věnuje
-SELECT f.jmeno, f.prijmeni, s.nazev, s.trener FROM fitkari f JOIN skupinovelekce s ON f.skupinovelekce_id = s.id;-- Vybere jméno a příjmení fitkaře, název skupinové lekce a jméno trenéra této lekce
+SELECT f.jmeno, f.prijmeni, s.nazev FROM fitkari f JOIN skupinovelekce s ON f.skupinovelekce_id = s.id; -- vybere jmeno a prijmeni fitkare a nazev skupinove lekce, na kterou chodi
+SELECT f.jmeno, f.prijmeni, b.druh FROM fitkari f JOIN bojovesporty b ON f.bojovesporty_id = b.id; -- vybere jmeno a prijmeni fitkare a druh bojového sportu, kteremu se venuje
+SELECT b.druh, z.jmeno, z.prijmeni FROM bojovesporty b JOIN zamestnanci z ON b.trener = CONCAT(z.jmeno, ' ', z.prijmeni); -- vybere druh bojoveho sportu a jmeno + prijmeni trenera, ktery ho vede
+SELECT s.nazev, z.jmeno, z.prijmeni FROM skupinovelekce s JOIN zamestnanci z ON s.trener = CONCAT(z.jmeno, ' ', z.prijmeni); -- vybere nazev skupinove lekce a jmeno + prijmeni trenera, ktery ji vede
+SELECT f.jmeno AS fitkar, b.trener FROM fitkari f JOIN bojovesporty b ON f.bojovesporty_id = b.id; -- vybere jmeno fitkare a jmeno trenera bojoveho sportu, kteremu se fitkar venuje
+SELECT f.jmeno, f.prijmeni, s.nazev, s.trener FROM fitkari f JOIN skupinovelekce s ON f.skupinovelekce_id = s.id; -- vybere jmeno a prijmeni fitkare, nazev skupinove lekce a jmeno trenera teto lekce
 
 -- 22-23: Dotazy s LEFT JOIN a RIGHT JOIN
 SELECT z.jmeno, z.prijmeni, s.nazev FROM zamestnanci z LEFT JOIN skupinovelekce s ON s.trener = CONCAT(z.jmeno, ' ', z.prijmeni);
